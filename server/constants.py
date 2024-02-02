ef6769a7825bdf11eff7213d9b339f2d733680cc
Freddy May <freddymay@mac.com>
2024-02-02 15:25:02 +0100

    new stuff

--- server/constants.py
+++ server/constants.py
@@ -2,5 +2,7 @@ SERVER_PORT = 33291
 
 CONTENT_TYPE_JSON = {"Content-Type": "application/json"}
 CONTENT_TYPE_HTML = {"Content-Type": "text/html; charset=utf-8"}
+CONTENT_TYPE_PLAIN_TEXT = {"Content-Type": "text/plain"}
 CONTENT_TYPE_IMAGE_PNG = {"Content-Type": "image/png"}
-CONTENT_TYPE_AUDIO_WEBM = {"Content-Type": "audio/webm"}
+CONTENT_TYPE_AUDIO_MPEG = {"Content-Type": "audio/mpeg"}
+CONTENT_TYPE_VIDEO_MP4 = {"Content-Type": "video/mp4"}
--- server/routes/mime-test.py
+++ server/routes/mime-test.py
@@ -11,25 +11,26 @@ def mime_test(namespace: str, mimetype: str):
     This endpoint returns sample data for the given mimetype.
     """
 
-    sample_files = {
-        'application/json': 'test.json',
-        'text/html': 'test.html',
-        'text/plain': 'test.txt',
-        'audio/mpeg': 'test.mp3',
-        'image/png': 'test.png',
-        'video/webm': 'test.webm'
-    }
+    if mimetype == 'image':
+#         return url_for('static', filename='test.png'), 200, CONTENT_TYPE_IMAGE_PNG
+        return 'https://picsum.photos/200/300', 200, CONTENT_TYPE_IMAGE_PNG
 
-    if mimetype == 'image/png':
-        return url_for('static', filename='test.png'), 200, CONTENT_TYPE_IMAGE_PNG
+    if mimetype == 'audio':
+       return url_for('static', filename='audio.mp3'), 200, CONTENT_TYPE_AUDIO_MPEG
 
-    if mimetype == 'audio/mpeg':
-       return url_for('static', filename='test.mp3'), 200, CONTENT_TYPE_IMAGE_PNG
+    if mimetype == 'video':
+       return url_for('static', filename='video.mp4'), 200, CONTENT_TYPE_VIDEO_MP4
 
-    if mimetype == 'video/webm':
-       return url_for('static', filename='test.webm'), 200, CONTENT_TYPE_AUDIO_WEBM
+    if mimetype == 'youtube':
+       return 'https://www.youtube.com/watch?v=j41bb5LdA5U', 200, CONTENT_TYPE_VIDEO_MP4
 
-    if mimetype in sample_files:
-        return send_from_directory("static", sample_files[mimetype])
+    if mimetype == 'text':
+       return send_from_directory("static", 'test.txt'), 200, CONTENT_TYPE_PLAIN_TEXT
+
+    if mimetype == 'html':
+       return send_from_directory("static", 'test.html'), 200, CONTENT_TYPE_HTML
+
+    if mimetype == 'json':
+       return send_from_directory("static", 'test.json'), 200, CONTENT_TYPE_JSON
 
     return json.dumps({"error": "No sample is available for this mimetype"}), 404, CONTENT_TYPE_JSON
